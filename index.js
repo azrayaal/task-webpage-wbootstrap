1e05c99f311c934118be6659fc2c54a2fe1e631e
azra <azrayazidalkautsar@gmail.com>
2023-08-25 17:52:45 +0700

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const path = require('path')
+const sendmail = require('sendmail')
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
@@ -11,9 +12,10 @@ app.listen(port, () => {
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+
 // set serving static file
 app.use(express.static('views'))
-
+// Middleware
 app.use(express.urlencoded({ extended: false }))
 
 
@@ -21,13 +23,14 @@ app.use(express.urlencoded({ extended: false }))
 // function router
 const home = (req, res) => {res.render('index', {title: 'Home'})}
 const blog = (req, res) => {res.render('blog',  {title: 'Blog'})}
-// const contact = (req, res) => {res.render('contact')}
+const contact = (req, res) => {res.render('contact', {title: 'Contact'})}
 
 // router
 app.get('/', home)
 app.get('/blog', blog)
-// app.get('/contact', contact)
+app.get('/contact', contact)
 app.post('/blog', addContentBlog)
+app.post('/contact', sendContact)
 
 // function blog
 function addContentBlog(req, res){
@@ -35,4 +38,40 @@ function addContentBlog(req, res){
   console.log("data: ", projectName, startDate, endDate, description)
   
   res.redirect('/blog')
+}
+
+
+// function contact
+function sendContact(req, res){
+  let  {name, email, phone, subject, message} = req.body
+
+  console.log("data: ", name, email, phone, subject, message)
+
+  
+
+// Email content
+// const mailOptions = {
+//     from: email,
+//     to: 'azrayazidalkautsar@gmail.com',
+//     subject: subject,
+//     text: message,
+// };
+
+// // Send the email
+// const sendmail = require('sendmail')();
+//     sendmail(mailOptions, (error, reply) => {
+//         if (error) {
+//             console.log(error);
+//             res.send('Error sending email.');
+//         } else {
+//             console.log('Email sent:', reply);
+//             res.send('Email sent successfully.');
+//         }
+//     });
+
+
+
+
+  res.redirect('/contact')
+  // console.log('info info');
 }
