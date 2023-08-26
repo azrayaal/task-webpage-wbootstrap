0c9b8cc48346dff7d196f014204c870dc6ccc6e3
azra <azrayazidalkautsar@gmail.com>
2023-08-26 22:21:51 +0700

    update

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express')
 const app = express()
 const port = 5000
 const path = require('path')
+const session = require('express-session')
+const flash = require('connect-flash');
+
 // const sendmail = require('sendmail')
 
 app.listen(port, () => {
@@ -15,91 +18,102 @@ app.set('views', path.join(__dirname, 'views'))
 
 // set serving static file
 app.use(express.static('views'))
+app.use(flash());
 // Middleware
 app.use(express.urlencoded({ extended: false }))
+app.use(session({
+  secret: 'a',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
 
 
 
 // function router
 // Home
 const home = (req, res) => {res.render('index', {title: 'Home', blogData })}
+
 // Blog
 const blog = (req, res) => {res.render('blog',  {title: 'Blog'})}
+const blogDetail = (req, res) => { 
+  const { id } = req.params 
+  res.render('blog-detail', 
+  {title: `Blog Detail ${id}`, blog: blogData[id]})
+}
 const addContentBlog = (req, res)=>{
   let  {title, technologies ,content} = req.body
   // buat fungsi add ID auto add
   let dataAddBlog = {
   id:10, title, technologies ,content
   }
+
   blogData.push(dataAddBlog);
   console.log("data addBlog: ", dataAddBlog)
   res.redirect('/')
 }
-const blogDetail = (req, res) => { 
+const viewBlogEdit=(req, res)=>{
   const { id } = req.params 
-  res.render('blog-detail', 
-  {title: `Blog Detail ${id}`, blog: blogData[id]})
+  res.render('blog-edit', {title: 'Edit Blog', editBlog: blogData[id]}, console.log(blogData[id]))
 }
 const blogEdit = (req, res) => { 
-  const { id } = req.params 
+  const editBlogId = parseInt(req.params.id)
+  const updateTitle = req.body.title
+  const updateContent = req.body.content
+  const updateTechnologies = req.body.technologies
+  // const updateImage = req.body.image
 
-  res.render('blog-edit', {title: 'Edit Blog', editBlog: blogData[id]}, console.log(blogData[id]))
+  const editData = blogData.find(editData => editData.id === editBlogId)
 
-  
-  const { title,  description, content } = req.body 
+  if(editData){
+      editData.title = updateTitle
+      editData.content = updateContent
+      editData.technologies = updateTechnologies
+      // editData.image = updateImage
 
-  let editDataBlog = {
-    title,  description, content
+      if(updateTitle === ''){
+        const failedMessage = req.flash('FAILED', 'Title must be filled!');
+        res.send({ failedMessage })
+        // req.flash('FAILED', 'Title must be filled!');
+        // res.redirect('/')
+      //  console.log('Title must be filled');
+      } else if (updateContent === ''){
+        res.send('CONTENT HARUS DI ISI')
+      } else if (updateTechnologies === ''){
+        res.send('TECHNOLOGIES HARUS DI PILIH')
+      }
+
+      res.redirect('/')
+  } else {
+    res.send('Failed to update new data')
   }
-
-  console.log('data dari Edit Blog: ', editDataBlog);
-
- 
-  res.redirect('/')
 }
 const blogDelete = (req, res) => { 
   const { id } = req.params 
   blogData.splice(id, 1)
   res.redirect('/')
 }
+
 // Contact
 const contact = (req, res) => {res.render('contact', {title: 'Contact'})}
 
 //////////// router
 // home
 app.get('/', home)
-// app.get('/', )
 // blog
 app.get('/blog', blog)
-app.get('/blog-detail/:id', blogDetail)
-app.get('/blog-delete/:id', blogDelete)
-app.get('/blog-edit/:id', blogEdit)
-app.put('/blog-edit/:id', blogEdit)
 app.post('/blog', addContentBlog)
+app.get('/blog-detail/:id', blogDetail)
+app.get('/blog-edit/:id', viewBlogEdit)
+app.post('/blog-edit/:id', blogEdit)
+app.get('/blog-delete/:id', blogDelete)
 // contact
 app.get('/contact', contact)
 app.post('/contact', sendContact)
 
+
 // /////////////////////////////////
-
-
-// function add blog
-// function addContentBlog(req, res){
-//   let  {title, technologies ,content} = req.body
-
-
-//   // buat fungsi add ID auto add
-  
-//   let dataAddBlog = {
-//   id:10, title, technologies ,content
-//   }
-
-//   blogData.push(dataAddBlog);
-
-//   console.log("data addBlog: ", dataAddBlog)
-//   res.redirect('/')
-// }
-
 
 // dummy data
 const blogData = [
