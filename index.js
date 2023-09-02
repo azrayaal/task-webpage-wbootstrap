ff8dd2e63e19925ae1a9faede82c5575140febff
azra <azrayazidalkautsar@gmail.com>
2023-09-02 20:02:04 +0700

    update rapihin layout

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ app.use(session({
 }))
 
 
-
 // call ejs as view engine
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'src/views'))
@@ -45,171 +44,8 @@ app.use(express.static('src/uploads'))
 // Middleware
 app.use(express.urlencoded({ extended: false }))
 
-// function router
 
-// Home
-// const home = async (req, res) => {
-//   try {
-//     const query = `SELECT * FROM blog;`
-//     let data = await sequelize.query(query, { type: QueryTypes.SELECT})
-    
-//     // console.log('home data: ', obj);
-    
-//     res.render('index', 
-//     {
-//     title: 'Home', 
-//     blogData: data, 
-//     isLogin: req.session.isLogin,
-//     user: req.session.user
-    
-//     // user: req.session.user
-//     })
-//     console.log('==========================================');
-//     console.log('isLogin at homepage a: ', req.session.isLogin, req.session.user);
-//     console.log('==========================================');
-
-//   } catch (error) {
-//     console.log('error bos', error);
-//   }
-// }
-// register
-// const viewRegister = (req, res) =>{
-//   res.render('register', {title: 'Register Page'})
-// }
-// const register = async(req, res) => {
-//   try {
-//     const {name, email, password} = req.body
-//     const saltRound = 10
-
-//     await bcrypt.hash(password, saltRound, (err, hashPassword)=>{
-//       const query = `INSERT INTO users (name, email, password) VALUES ('${name}', '${email}', '${hashPassword}')`
-//       sequelize.query(query)
-//     })
-//     res.redirect('/')
-//     console.log('data from register: ', dataHash);
-//   } catch (error) {
-//     console.log('error from register page: ', error);
-//   }
-// }
-// login
-// const logOut = (req, res) =>{
-//   req.session.destroy()
-//   res.redirect('/')
-// }
-// const viewLogin = (req, res) =>{
-//   res.render('login', {title: 'Login Page'})
-// }
-// const login = async(req, res)=>{
-//   try {
-//     const {email, password} = req.body
-//     const query = `SELECT * FROM users WHERE email = '${email}'`
-
-//     const dataUsers = await sequelize.query(query, {type: QueryTypes.SELECT})
-
-//     console.log("data objek: ", dataUsers[0].password);
-
-//     if(!dataUsers.length) {
-//       req.flash = ('danger', 'user has not been registered')
-//     }
-
-//     await bcrypt.compare(password, dataUsers[0].password, (err, result)=>{
-//       if(!result){
-//         req.flash('danger', 'password wrong')
-//         return res.redirect('/login')
-//       } else {
-//         const isLogin = req.session.isLogin = true
-//         // req.session.isLogin = true
-//         req.session.user = dataUsers[0].name
-//         isLogin
-//         req.flash('succes', 'login berhasil')
-//         console.log('==========================================');
-//         console.log('BERHASIL LOGIN: ', isLogin, dataUsers[0].name);
-//         console.log('==========================================');
-//         res.redirect('/')
-//       }
-//     })
-//     console.log('data from login page: ', dataUsers);
-//   } catch (error) {
-//     console.log('error from login page: ', error);
-//   }
-// }
-
-// Blog
-// const blog = (req, res) => {res.render('blog',  {title: 'Blog'})}
-// const blogDetail = async(req, res) => { 
-//   try {
-//     const {id} = req.params
-//     const query = (`SELECT * FROM blog WHERE id =${id}`)
-//     const obj = await sequelize.query(query, {type: QueryTypes.SELECT})
-
-//     const data = obj.map((item)=>({
-//       ...item
-//     }))
-    
-//      console.log('data obj detail: ', data);
-//     res.render('blog-detail', {title: 'Detail Blog', blog: data[0]})
-   
-//   } catch (error) {
-//     console.log("error from blog-detail: ", error);
-//   }
-// }
-// const addContentBlog = async(req, res)=>{
-//   try {
-//     let  {title, start_date, end_date, content, technologies} = req.body
-//     const image = '/img/katheryne-card.png'
-
-//     await sequelize.query(`INSERT INTO blog(title, start_date, end_date, content, technologies, image) VALUES ('${title}', '${start_date}', '${end_date}', '${content}', ARRAY ['${technologies}'], '${image}')`)
-
-//     console.log("data addBlog: ", title, start_date, end_date, content, technologies)
-//     res.redirect('/')
-//   } catch (error) {
-//     console.log("ERROR BOS: ", error);
-//   }
-// }
-const viewBlogEdit = async(req, res)=>{
-  try {
-    const {id} = req.params
-    const query = `SELECT * FROM blog WHERE id =${id};`
-    let obj = await sequelize.query(query, { type: QueryTypes.SELECT})
-
-    const data = obj.map((item)=>({
-      ...item
-    }))
-    
-    console.log('objdb detail: ', obj);
-    res.render('blog-edit', {title: 'edit page', editBlog: data[0]})
-  } catch (error) { 
-    console.log('error bos: ', error);
-  }
-  
-}
-const blogEdit = async(req, res) => { 
-  try {
-    const {id} = req.params
-    let{
-      title, start_date, end_date, content, technologies
-    } = req.body
-
-    await sequelize.query(`UPDATE blog SET title = '${title}', start_date= '${start_date}', end_date='${end_date}', content='${content}' WHERE id = ${id} `)
-    console.log('data update: ', title, start_date, end_date, content, technologies);
-    res.redirect('/')
-    } catch (error) {
-      console.log('error', error);
-  }
-}
-const blogDelete = async(req, res) => { 
-try {
-  const { id } = req.params 
-  await sequelize.query(`DELETE FROM blog WHERE id = ${id}`)
-      res.redirect('/')
-    } catch (error) {
-      console.log('error: ', error);
-    }
-}
-// Contact
-const contact = (req, res) => {res.render('contact', {title: 'Contact'})}
-
-//////////// router
+//////////////////  ROUTER  ////////////////////////////////
 // home
 app.get('/', home)
 app.get('/logout', logOut)
@@ -230,113 +66,7 @@ app.get('/blog-delete/:id', blogDelete)
 app.get('/contact', contact)
 app.post('/contact', sendContact)
 
-
-// /////////////////////////////////
-
-// const technologyIcons = {
-//   'ExpressJS': 'fab fa-node-js',
-//   'NodeJS': 'fab fa-node-js'
-// };
-// dummy data
-const blogData = [
-  { id: 0, 
-    title: "Blog 0",
-    content: 'Blog ipsum dolor amet data 0',
-    technologies: ['NodeJs', 'ReactJs'],
-    image: '/img/katheryne-card.png',
-    startDate: '2023-08-28',
-    endDate: '2023-08-31',
-    postAt: '2023-08-28T02:03:13.424Z'
-  },
-  // { id: 1, 
-  //   title: "Blog 1",
-  //   content: 'data 1 Blog ipsum dolor amet data 2',
-  //   technologies: ['ReactJs', 'NodeJs', 'Go'],
-  //   image: '/img/chef-mao-card.png',
-  //   startDate: '2023-08-28',
-  //   endDate: '2023-08-31',
-  //   postAt: '2023-08-28T02:03:13.424Z'
-  // },
-  // { id: 2, 
-  //   title: "Blog 2",
-  //   content: 'data 2 Blog ipsum dolor amet data 3',
-  //   technologies: ['Go', 'NodeJs'],
-  //   image: '/img/chang-the-ninth-card.png',
-  //   startDate: '2023-08-28',
-  //   endDate: '2023-08-31',
-  //   postAt: '2023-08-28T02:03:13.424Z'
-  // },
-  // { id: 3, 
-  //   title: "Blog 3",
-  //   content: 'data 3 Blog ipsum dolor amet data 4',
-  //   technologies: ['ReactJs'],
-  //   image: '/img/tian.png',
-  //   startDate: '2023-08-28',
-  //   endDate: '2023-08-31',
-  //   postAt: '2023-08-28T02:03:13.424Z'
-  // },
-  // { id: 4, 
-  //   title: "Blog 4",
-  //   content: 'data 4 Blog ipsum dolor amet data 4',
-  //   technologies: ['ReactJs', 'Go'],
-  //   image: '/img/chang-the-ninth-card.png',
-  //   startDate: '2023-08-28',
-  //   endDate: '2023-08-31',
-  //   postAt: '2023-08-28T02:03:13.424Z'
-  // },
-];
-// TECH
-// array satu ga masuk, tapi dua masuk
-
-
-
-
-
-
-
-
-
-
-
-
-// function contact
-function sendContact(req, res){
-  let  {name, email, phone, subject, message} = req.body
-
-  console.log("data: ", name, email, phone, subject, message)
-
-  
-
-// Email content
-// const mailOptions = {
-//     from: email,
-//     to: 'azrayazidalkautsar@gmail.com',
-//     subject: subject,
-//     text: message,
-// };
-
-// // Send the email
-// const sendmail = require('sendmail')();
-//     sendmail(mailOptions, (error, reply) => {
-//         if (error) {
-//             console.log(error);
-//             res.send('Error sending email.');
-//         } else {
-//             console.log('Email sent:', reply);
-//             res.send('Email sent successfully.');
-//         }
-//     });
-
-
-
-
-  res.redirect('/contact')
-  // console.log('info info');
-}
-
-
-
-// //////////////////////////////////////////////////////
+//////////////////  FUNCTION  ////////////////////////////////
 ///////////////  HOME  /////////////// 
 async function home(req, res){ 
   try {
@@ -429,14 +159,18 @@ function logOut(req, res){
 }
 ///////////////  BLOG  ///////////////
 function viewFormBlog(req, res){
-  res.render('blog', {title: 'Blog Page'})
+  res.render('blog', {
+    title: 'Blog Page',
+    isLogin: req.session.isLogin,
+    user: req.session.user
+  })
 }
 async function addContentBlog(req, res){
   try {
   const {title, content, technologies, start_date, end_date} = req.body
   const image = '/img/katheryne-card.png'
 
-  await sequelize.query(`INSERT INTO blog (title, content, technologies, start_date, end_date) VALUES ('${title}', '${content}', ARRAY ['${technologies}'], '${start_date}', '${end_date}', '${image})`)
+  await sequelize.query(`INSERT INTO blog (title, content, technologies, start_date, end_date, image) VALUES ('${title}', '${content}', ARRAY ['${technologies}'], '${start_date}', '${end_date}', '${image}')`)
   
   res.redirect('/')
   } catch (error) {
@@ -458,7 +192,9 @@ async function viewBlogDetail(req, res){
 
    res.render('blog-detail', {
     title: 'Blog Detail Page', 
-    blog: data[0]
+    blog: data[0],
+    isLogin: req.session.isLogin,
+    user: req.session.user
    })
 
   } catch (error) {
@@ -467,3 +203,68 @@ async function viewBlogDetail(req, res){
     console.log('==========================================');
   }
 }
+async function viewBlogEdit(req, res){
+  try {
+    const {id} = req.params
+    const query = `SELECT * FROM blog WHERE id = ${id}`
+
+    const blog = await sequelize.query(query, {type: QueryTypes.SELECT})
+
+    const data = blog.map((item)=>({
+      ...item
+    }))
+
+    res.render('blog-edit', {
+      title: 'Blog Edit', 
+      editBlog: data[0],
+      isLogin: req.session.isLogin,
+      user: req.session.user
+  })
+    
+  } catch (error) {
+    console.log('==========================================');
+    console.log('error from PROCESS Blog Edit Page: ', error);
+    console.log('==========================================');
+  }
+}
+async function blogEdit(req, res){
+  try {
+    const {id} = req.params
+    const {title, start_date, end_date, technologies} = req.body
+
+    await sequelize.query(`UPDATE blog SET title= '${title}', start_date= '${start_date}', end_date= '${end_date}' WHERE id= '${id}'`)
+
+    req.flash('success', 'Blog successfuly updated')
+    res.redirect('/')
+  } catch (error) {
+    console.log('==========================================');
+    console.log('error from PROCESS Blog Edit Process: ', error);
+    console.log('==========================================');
+  }
+}
+async function blogDelete(req, res){
+  try {
+    const {id} = req.params
+    await sequelize.query(`DELETE FROM blog WHERE id= ${id}`)
+    req.flash('success', 'Blog succesfully removed')
+    res.redirect('/')
+  } catch (error) {
+    console.log('==========================================');
+    console.log('error from PROCESS Blog Delete Process: ', error);
+    console.log('==========================================');
+  }
+}
+///////////////  CONTACT  ///////////////
+function contact(req, res){
+  res.render('contact', {
+    title: 'Contact Page',
+    isLogin: req.session.isLogin,
+    user: req.session.user
+  })
+}
+function sendContact(req, res){
+  const {name, email, phone, subject, message} = req.body
+  // console.log("data: ", name, email, phone, subject, message)
+  res.redirect('/contact')
+}
+
